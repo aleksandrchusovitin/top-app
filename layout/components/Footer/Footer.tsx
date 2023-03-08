ff6b6ed1f1ef892f9e66e45bbf883c3dc6ef4d52
Aleksandr Chusovitin <aleksandr.chusovitin@dualbootpartners.com>
2023-03-08 18:58:54 +0700

    styled footer

--- layout/components/Footer/Footer.tsx
+++ layout/components/Footer/Footer.tsx
@@ -1,10 +1,34 @@
 import { FC } from 'react';
 
 import { IFooterProps } from './Footer.props';
-// import styles from './Footer.module.css';
+import styles from './Footer.module.css';
+import clsx from 'clsx';
+import { format } from 'date-fns';
 
 export const Footer: FC<IFooterProps> = (props) => {
-  const { ...otherProps } = props;
+  const { className, ...otherProps } = props;
 
-  return <footer {...otherProps}>Footer</footer>;
+  return (
+    <footer className={clsx(className, styles.root)} {...otherProps}>
+      <p className={styles.allRights}>
+        OwlTop © 2020 - {format(new Date(), 'yyyy')} Все права защищены
+      </p>
+      <a
+        href='#'
+        target='_blank'
+        rel='noopener noreferrer'
+        className={styles.userAgreement}
+      >
+        Пользовательское соглашение
+      </a>
+      <a
+        href='#'
+        target='_blank'
+        rel='noopener noreferrer'
+        className={styles.policy}
+      >
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };
